cb22ed4fa69badca898297e830e894367172d6a9
Arthur Costa <krashcontas@gmail.com>
2025-08-21 20:55:49 -0300

    handlebars

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const exphbs = require('express-handlebars');
 const db = require('./db/connection');
 const bodyParser = require('body-parser');
+const path = require('path');
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
 
 //body parser
 app.use(bodyParser.urlencoded({ extended: false }));
+
+//handle bars
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //db connection
@@ -22,7 +33,7 @@ db.authenticate()
 
 //routes
 app.get('/', (req, res) => {
-    res.send('Esta funcionando');
+    res.render('index')
 });
 
 //jobs routes
